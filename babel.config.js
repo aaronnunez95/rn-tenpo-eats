be14abf5ba6d3426c5b226f5ff93e98a6be62f3a
aaronnunez95 <aaronplate95@gmail.com>
2021-08-11 10:12:23 -0500

    [ADD] Module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,19 @@ module.exports = {
   plugins: [
     [
       'dotenv-import', { moduleName: '@env' }
+    ],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@assets'    : './src/assets',
+          '@components': './src/components',
+          '@lib'       : './src/lib',
+          '@screens'   : './src/screens'
+        },
+        extensions: [ '.js', '.ios.js', '.android.js' ],
+        root      : [ './src' ]
+      }
     ]
   ],
   presets: [ 'module:metro-react-native-babel-preset' ]
