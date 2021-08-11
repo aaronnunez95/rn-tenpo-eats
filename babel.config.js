cf6cf5ac265745e8589cd84fe884a7a78a8d86d2
aaronnunez95 <aaronplate95@gmail.com>
2021-08-11 02:45:04 -0500

    [ADD] Configure enviroment (ENV) variables

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'dotenv-import', { moduleName: '@env' }
+    ]
+  ],
+  presets: [ 'module:metro-react-native-babel-preset' ]
+}
